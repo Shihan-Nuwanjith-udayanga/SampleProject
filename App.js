787fb78a4058a4bb3d3d2aa2a7bd703d4f737bff
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-10-10 22:02:26 +0530

    preview the Welcome Image

--- App.js
+++ App.js
@@ -7,52 +7,10 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => Node = () => {
-  return (
-    <ScrollView>
-      <View>
-        <Text>Hello App</Text>
-      </View>
-    </ScrollView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+function App() {
+  return <WelcomeScreen />;
+}
 
 export default App;
