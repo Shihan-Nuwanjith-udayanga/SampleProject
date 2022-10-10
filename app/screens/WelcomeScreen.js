f8599a84120e93e7b180f37b4b2734f73598338b
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-10-10 22:13:25 +0530

    add login and register button

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import { ImageBackground , StyleSheet } from "react-native";
+import {ImageBackground, StyleSheet, View} from 'react-native';
 
 function WelcomeScreen(props) {
-    return (
-        <ImageBackground 
-            style = {styles.background}
-            source={require("../assets/background.jpg")}
-        ></ImageBackground>
-    );
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require('../assets/background.jpg')}>
+      <View style={styles.loginButton}></View>
+      <View style={styles.RegisterButton}></View>
+    </ImageBackground>
+  );
 }
 
 const styles = StyleSheet.create({
-    background: {
-        flex: 1,
-    }
-})
+  background: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+
+  loginButton: {
+    width: '100%',
+    height: 70,
+    backgroundColor: '#fc5c65',
+  },
+
+  RegisterButton: {
+    width: '100%',
+    height: 70,
+    backgroundColor: '#4ecdc4',
+  },
+});
 
 export default WelcomeScreen;
