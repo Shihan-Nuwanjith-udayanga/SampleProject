92528337255518174876f5df1575ec6acf5d7b78
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-10-11 07:26:50 +0530

    change welcomeScreen logo and background image

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -9,7 +9,7 @@ function WelcomeScreen(props) {
       <View style={styles.logoContainer}>
         <Image
           style={styles.logo}
-          source={require('../assets/academic_logo.jpg')}></Image>
+          source={require('../assets/logo-red.png')}></Image>
         <Text>Sell What You Don't Need</Text>
       </View>
       <View style={styles.loginButton}></View>
