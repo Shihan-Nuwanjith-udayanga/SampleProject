47e40a75e639d2243eda53233a912014e29e0754
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-10-12 13:59:51 +0530

    add touchable component

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,42 +1,51 @@
 import React from 'react';
-import { View, StyleSheet, Image } from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Image,
+  TouchableHighlight,
+  TouchableOpacity,
+} from 'react-native';
 
 import AppText from './AppText.js';
 import colors from '../config/colors.js';
 
-function ListItem({title, subTitle, image}) {
-    return (
-        <View style={styles.container}>
-            <Image style={styles.image} source={image} />
-            <View>
-                <AppText style={styles.title}>{title}</AppText>
-                <AppText style={styles.subTitle}>{subTitle}</AppText>
-            </View>
+function ListItem({title, subTitle, image, onPress}) {
+  return (
+    <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
+      {/* // underlayColor={colors.light} */}
+      {/* // onPress={() => console.log()} */}
+      <View style={styles.container}>
+        <Image style={styles.image} source={image} />
+        <View>
+          <AppText style={styles.title}>{title}</AppText>
+          <AppText style={styles.subTitle}>{subTitle}</AppText>
         </View>
-    );
+      </View>
+    </TouchableHighlight>
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flexDirection: 'row',
-        padding: 15,
-    },
+  container: {
+    flexDirection: 'row',
+    padding: 15,
+  },
 
-    image: {
-        width: 70,
-        height: 70,
-        borderRadius: 35,
-        marginRight: 10
-    },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    marginRight: 10,
+  },
 
-    title:{
-        fontWeight: '500',
-    },
+  title: {
+    fontWeight: '500',
+  },
 
-    subTitle: {
-        color: colors.medium,
-    }
-
+  subTitle: {
+    color: colors.medium,
+  },
 });
 
 export default ListItem;
