205a6fb7ed65b25498deec6b8036703c42da60b4
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-10-12 11:36:39 +0530

    create ListingDetails Screen

---
+++ app/components/ListingDetailsScreen.js
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+function ListingDetailsScreen() {
+  return (
+    <View>
+      <Text>ListingDetailsScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    
+});
+
+export default ListingDetailsScreen;
