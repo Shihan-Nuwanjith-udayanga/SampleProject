8e4cf89c04da4822cb4fc26d0e394216399d370e
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-10-12 11:07:44 +0530

    create a HomeScreen.js file

---
+++ app/screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import default from './../config/colors';
+
+function HomeScreen() {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+    
+});
+
+export default HomeScreen;
