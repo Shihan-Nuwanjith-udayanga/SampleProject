89139e31d6b6f7e263de9bd3cb748a40eef2974d
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-10-12 11:33:15 +0530

    create a card

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,17 +1,46 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 
-function Card(props) {
+import colors from '../config/colors.js';
+import AppText from './AppText';
+
+function Card({title, subTitle, image}) {
   return (
-    <View>
-      
+    <View style={styles.card}>
+      <Image style={styles.image} source={image} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>{title}</AppText>
+        <AppText style={styles.subTitle}>{subTitle}</AppText>
+      </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  
-    
+  card: {
+    borderRadius: 15,
+    backgroundColor: colors.white,
+    marginBottom: 20,
+    overflow: 'hidden'
+  },
+
+  image: {
+    width: '100%',
+    height: 200,
+  },
+
+  detailsContainer: {
+    padding: 20,
+  },
+
+  title: {
+    marginBottom: 7,
+  },
+
+  subTitle: {
+    color: colors.secondary,
+    fontWeight: 'bold',
+  },
 });
 
 export default Card;
