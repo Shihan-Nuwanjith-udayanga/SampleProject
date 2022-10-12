95bc41d67a8b923fa7151e6172552ece3d9e9249
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-10-12 11:32:57 +0530

    create view

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,18 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import default from './../config/colors';
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+import Card from '../components/Card';
 
 function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View
+      style={{
+        backgroundColor: '#f8f4f4',
+        padding: 20,
+        paddingTop: 100,
+      }}>
+      <Card
+        title="Red Jacket for sale"
+        subTitle="$100"
+        image={require("../assets/jacket.jpg")}
+      />
     </View>
-  )
+  );
 }
 
-
-const styles = StyleSheet.create({
-    
-});
+const styles = StyleSheet.create({});
 
 export default HomeScreen;
