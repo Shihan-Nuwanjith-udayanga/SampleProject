44039d25795f2dcf004f50074b0795924a56d674
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-10-12 11:16:04 +0530

    create new component "Card.js"

---
+++ app/components/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+function Card(props) {
+  return (
+    <View>
+      
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  
+    
+});
+
+export default Card;
